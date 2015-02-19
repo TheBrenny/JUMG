83499a44a0e86981aabd49a214b3966f24b4f7b8
mastercork889 <robinjars@gmail.com>
2015-02-20 08:38:54 +0930

    Whoops, fixed the inventory and items now. It's just basic...

--- JUMG/src/com/brennytizer/jumg/entities/items/Inventory.java
+++ JUMG/src/com/brennytizer/jumg/entities/items/Inventory.java
@@ -4,6 +4,49 @@ public class Inventory {
 	public Item[] inventory;
 	
 	public Inventory() {
-		
+		this(new Item[] {});
+	}
+	
+	public Inventory(Item[] inventory) {
+		this.inventory = inventory;
+	}
+	
+	public Item[] getInventory() {
+		return this.inventory;
+	}
+	
+	public boolean contains(String name) {
+		for(Item a : inventory)
+			if(a.getName().equals(name)) return true;
+		return false;
+	}
+	
+	public boolean contains(short itemID) {
+		for(Item a : inventory)
+			if(a.getItemID() == itemID) return true;
+		return false;
+	}
+	
+	public boolean contains(Item i, boolean byName) {
+		if(byName) return contains(i.getName());
+		else return contains(i.getItemID());
+	}
+	
+	public Item getItemAt(int index) {
+		return inventory[index];
+	}
+	
+	public Item[] resizeInventory(int size) {
+		Item[] newItems = new Item[size];
+		if(size > inventory.length) {
+			for(int i = 0; i < inventory.length; i++)
+				newItems[i] = inventory[i];
+		} else if(size < inventory.length) {
+			for(int i = 0; i < size; i++)
+				newItems[i] = inventory[i];
+		} else {
+			return this.inventory;
+		}
+		return newItems;
 	}
 }
--- JUMG/src/com/brennytizer/jumg/entities/items/Item.java
+++ JUMG/src/com/brennytizer/jumg/entities/items/Item.java
@@ -7,27 +7,44 @@ public abstract class Item {
 	public int stackCount;
 	public int maxStackCount;
 	
-	public Item(String name, short itemID, int startStackCount, int maxStackCount) throws DuplicateItemException {
-		this.name = name;
-		this.itemID = itemID;
-		this.stackCount = startStackCount;
-		this.maxStackCount = maxStackCount;
+	public Item(String name, short itemID, int startStackCount, int maxStackCount) {
 		if(Item.allItems[itemID] != null) {
-			throw new DuplicateItemException(itemID, allItems[itemID], this);
+			this.name = allItems[itemID].name;
+			this.itemID = allItems[itemID].itemID;
+			this.stackCount = allItems[itemID].stackCount;
+			this.maxStackCount = allItems[itemID].maxStackCount;
+		} else {
+			this.name = name;
+			this.itemID = itemID;
+			this.stackCount = startStackCount;
+			this.maxStackCount = maxStackCount;
 		}
 	}
 	
-	public class DuplicateItemException extends Exception {
-		private static final long serialVersionUID = 1L;
-		
-		public int id;
-		public Item first;
-		public Item second;
-		
-		public DuplicateItemException(int id, Item first, Item second) {
-			this.id = id;
-			this.first = first;
-			this.second = second;
-		}
+	public short getItemID() {
+		return itemID;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public int getStackCount() {
+		return stackCount;
+	}
+	
+	public int getMaxStackCount() {
+		return maxStackCount;
+	}
+	
+	public Item getItem(short id) {
+		if(id < 0 || id > 255) return null;
+		return allItems[id];
+	}
+	
+	public Item getItem(String name) {
+		for(Item i : allItems)
+			if(i.getName().equals(name)) return i;
+		return null;
 	}
 }
